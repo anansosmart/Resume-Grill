29bd6101b13eeb8fa86d60cb1b2ab5953801310c
Chester <2177007379@qq.com>
2026-07-21 17:52:54 +0800

    Create find_free_port.py

---
+++ scripts/find_free_port.py
@@ -0,0 +1,9 @@
+"""Print an available localhost TCP port for the Windows launcher."""
+
+from __future__ import annotations
+
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind(("127.0.0.1", 0))
+    print(sock.getsockname()[1])
